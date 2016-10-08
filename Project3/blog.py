76a2acb03602b74d5f9362ac9804b231f75d9596
Administrador <administrador@localhost.localdomain>
2016-10-08 23:03:37 +0200

    Initial commit Project3

---
+++ Project3/blog.py
@@ -0,0 +1,27 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Enviroment(loader = jinja2.FileSystemLoader(template_dir), 
+	autoescape = True)
+
+class BlogHandler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(BlogHandler):
+	def get(self):
+		self.write('Hello, Udacity!')
